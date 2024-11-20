b39d0bf5b90b57e94d89a44b41b2192d04cadcb6
Antonio Saqueton <code.antoniohicetasaqueton@gmail.com>
2024-11-20 14:34:28 +0800

    Implement loader and error components

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable react/prop-types */
+import { formatCurrency } from '../../utilities/helpers';
+
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
