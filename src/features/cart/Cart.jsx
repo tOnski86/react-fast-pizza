6877e5ddcae8ee2d0d0640884a536c6abf3073ac
Antonio Saqueton <code.antoniohicetasaqueton@gmail.com>
2024-11-24 12:53:08 +0800

    Add static page styles

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import Button from "../../ui/Button";
+import CartItem from "./CartItem";
 
 import LinkButton from "../../ui/LinkButton";
 
@@ -31,15 +32,23 @@ function Cart() {
   const cart = fakeCart;
 
   return (
-    <div>
+    <div className="px-4 py-3">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2>Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
 
-      <div>
-        <Button to="/order/new">Order pizzas</Button>
+      <ul className="mt-3 divide-y divide-stone-200 border-b">
+        {cart.map((item) => (
+          <CartItem item={item} key={item.key} />
+        ))}
+      </ul>
 
-        <button>Clear cart</button>
+      <div className="mt-6 space-x-2">
+        <Button to="/order/new" type="primary">
+          Order pizzas
+        </Button>
+
+        <Button type="secondary">Clear cart</Button>
       </div>
     </div>
   );
--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -19,7 +19,7 @@ function CreateUser() {
 
       {username !== "" && (
         <div>
-          <Button>Start Ordering</Button>
+          <Button type="primary">Start Ordering</Button>
         </div>
       )}
     </div>
